ec16abb85b7149f776444d0e5c03354551555c25
NIELI <1544944630@qq.com>
2020-07-06 14:23:08 +0800

    2017012313

---
+++ examOnline/src/test/dao/StudentDaoImplTest.java
@@ -0,0 +1,32 @@
+package test.dao;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.dao.StudentDaoImpl;
+import com.entity.Student;
+
+public class StudentDaoImplTest {
+	static StudentDaoImpl studentDao ;
+	static Student student = new Student();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		studentDao = new StudentDaoImpl();
+		student.setStudentID("1321152232");
+		student.setStudentName("吴俊川");
+		student.setPassword("wjc#940916");
+		student.setResult(100);
+		student.setSclass("移动云办公");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	@Test
+	public void testUpdate(){
+		
+		studentDao.save(student);
+	}
+}
